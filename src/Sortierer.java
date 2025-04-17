856cf47cee32ae800b2cd3162a5fa088d02472b2
Asa <ariishi66@gmail.com>
2025-04-17 11:46:13 +0200

    Added Sortierer.java class (Crying inside head hurt)

---
+++ src/Sortierer.java
@@ -0,0 +1,26 @@
+ import java.util.*;
+
+public abstract class Sortierer {
+    private final List<Integer> liste;
+    private long startZeit;
+    private long endZeit;
+
+    public Sortierer(List<Integer> input){
+        this.liste = new ArrayList<>(input);
+    }
+
+    protected abstract List<Integer> sortiere(List<Integer> toSort);
+
+    public List<Integer> sortiere(){
+        startZeit = System.currentTimeMillis();
+        List<Integer> result = sortiere(liste);
+        endZeit = System.currentTimeMillis();
+
+        return result;
+    }
+
+     public long getOperations(){
+        return endZeit - startZeit;
+     }
+
+}
