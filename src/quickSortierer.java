968ab7c10192128c65385cc70890bd0d71a9e48f
Asa <ariishi66@gmail.com>
2025-05-14 18:41:11 +0200

    quickSortierer

--- src/quickSortierer.java
+++ src/quickSortierer.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class quickSortierer extends Sortierer {
@@ -8,6 +9,28 @@ public class quickSortierer extends Sortierer {
 
     @Override
     protected List<Integer> sortiere(List<Integer> toSort) {
-        return List.of();
+        System.out.println("[DEBUG] quickSortierer.sortiere");
+
+        if(toSort.size() <= 1) return toSort;
+
+        int pivot = toSort.get(toSort.size()/2);
+        System.out.println("[DEBUG] Pivot: " + pivot);
+        List<Integer> less = new ArrayList<>(); //Elements smaller than pivot
+        List<Integer> greater = new ArrayList<>(); //Elements greater than pivot
+        List<Integer> equal = new ArrayList<>(); //Elements equal to pivot
+
+        //partitioning
+        for(int num : toSort) {
+            if (num < pivot) less.add(num);
+            else if (num > pivot) greater.add(num);
+            else equal.add(num);
+        }
+
+        List<Integer> sorted = new ArrayList<>();
+        sorted.addAll(sortiere(less));
+        sorted.addAll(equal);
+        sorted.addAll(sortiere(greater));
+
+        return sorted;
     }
 }
